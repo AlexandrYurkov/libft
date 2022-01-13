e37dabd68c82b8cc6949e4bcfa9b89eb6510a522
Laurinda Fallon <lfallon@ox-i5.msk.21-school.ru>
2022-01-13 17:07:56 +0300

    sort

--- liba/ft_strsplit.c
+++ liba/ft_strsplit.c
@@ -1,5 +1,72 @@
 #include "libft.h"
 
+size_t	ft_strnlen(const char *s, size_t maxlen)
+{
+	size_t	len;
+
+	len = 0;
+	while (len < maxlen)
+	{
+		if (!*s)
+			break ;
+		len++;
+		s++;
+	}
+	return (len);
+}
+
+char	*ft_strcpy(char *dest, const char *src)
+{
+	int i;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+unsigned int	ft_wc(char const *s, char c)
+{
+	unsigned int	wc;
+
+	wc = 0;
+	while (*s && *s == c)
+		s++;
+	while (*s)
+	{
+		while (*s && *s != c)
+			s++;
+		wc++;
+		while (*s && *s == c)
+			s++;
+	}
+	return (wc);
+}
+
+void	ft_matfree(char **str, int i)
+{
+	while (--i >= 0)
+		free(str[i]);
+	free(str);
+	str = NULL;
+}
+
+char	*ft_strdup(const char *src)
+{
+	char	*dup;
+
+	dup = malloc(ft_strlen(src) + 1);
+	if (dup == 0)
+		return (NULL);
+	ft_strcpy(dup, src);
+	return (dup);
+}
+
+
 char	**ft_strsplit(char const *s, char c)
 {
 	char		**str;
